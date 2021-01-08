06a28e41336a27d3f4d6a0ac4673641d9a1d2213
Giacomo Cerquone <cerquone96@hotmail.it>
2021-01-08 23:15:04 +0100

    Updates

---
+++ precache-manifest.acab1e71c9e56b16759f6f3c5d4e99c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa70b4dd971fac9a888e3bcd275c329a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "585eaae0f4499b52ef89",
+    "url": "./static/css/2.67731326.chunk.css"
+  },
+  {
+    "revision": "2cf1b02fe10a542928cf",
+    "url": "./static/css/main.4652f8b8.chunk.css"
+  },
+  {
+    "revision": "585eaae0f4499b52ef89",
+    "url": "./static/js/2.167a8eeb.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./static/js/2.167a8eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cf1b02fe10a542928cf",
+    "url": "./static/js/main.3988d60e.chunk.js"
+  },
+  {
+    "revision": "ae36c80f3117b37aa19d",
+    "url": "./static/js/runtime-main.6b766170.js"
+  }
+]);
