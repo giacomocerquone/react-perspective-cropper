9e2400e17c0c417530346ea33abcc34b151f811f
castlefurnitureworks@gmail.com <castlefurnitureworks@gmail.com>
2021-06-01 13:17:34 -0700

    updating types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,20 +1,41 @@
-import * as React from 'react'
+import React from "react";
+declare module "react-perspective-cropper" {
+  export interface Cropper extends React.FC<CropperProps> {}
 
-export interface CropperProps {
-  image: string | File
-  onDragStop: () => void
-  onChange: () => void
-  cropperRef: React.ElementRef
-  pointSize: number
-  lineWidth: number
-  pointBgColor: string
-  pointBorder: string
-  lineColor: string
-  maxWidth: number
-  maxHeight: number
-  openCvPath: string
+  export interface CropperProps {
+    image: File | null;
+    onDragStop: () => void;
+    onChange: () => void;
+    cropperRef: React.ElementRef;
+    pointSize: number;
+    lineWidth: number;
+    pointBgColor: string;
+    pointBorder: string;
+    lineColor: string;
+    maxWidth: number;
+    maxHeight: number;
+    openCvPath: string;
+  }
+
+  export interface CropperRef extends React.ElementRef {
+    backToCrop: () => void;
+    /**
+     * function that does the transforming, filtering, and optionally shows a preview
+     */
+    done: (options: Partial<ClickCropOptions>) => Promise<Blob>;
+  }
+
+  interface ClickCropOptions {
+    preview: boolean;
+    filterCvParams: Partial<OpenCVFilterProps>;
+  }
+  interface OpenCVFilterProps {
+    blur: boolean;
+    th: boolean;
+    thMode: any;
+    thMeanCorrection: number;
+    thBlockSize: number;
+    thMax: number;
+    grayScale: boolean;
+  }
 }
-
-const Cropper: React.FC<CropperProps>
-
-export default Cropper
